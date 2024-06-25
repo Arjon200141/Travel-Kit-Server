2dc86b4ab83afd548300ecf6981ea1bf6be31a4d
Arjon200141 <aorjon123@gmail.com>
2024-06-25 14:19:08 +0600

    Card Load and Carts get added

--- index.js
+++ index.js
@@ -24,20 +24,47 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db('travel_kit').collection('products');
+    const reviewsCollection = client.db('travel_kit').collection('reviews');
+    const cartCollection = client.db('travel_kit').collection('carts');
+
+    app.get('/products', async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    })
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.post('/carts', async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("Travel Kit is Running");
+  res.send("Travel Kit is Running");
 })
 app.listen({ port }, () => {
-console.log(`Travel Kit is Running at ${port}`);
+  console.log(`Travel Kit is Running at ${port}`);
 })
